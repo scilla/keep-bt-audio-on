f914e820676ed3b96ddcd09110460b2b186331fd
scilla <sr.cilla@gmail.com>
2024-10-15 12:12:06 +0200

    fix: add missing dependencies and assets

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup
 
-APP = ['main.py']
+APP = ["main.py"]
 OPTIONS = {
-	'argv_emulation': True,
-	'plist': {
-		'LSUIElement': True,
-	},
-	'packages': ['rumps', 'pydub'],
+    "argv_emulation": False,
+    "plist": {
+        "LSUIElement": True,
+    },
+    "packages": ["rumps", "pydub", "subprocess", "threading", "tempfile"],
 }
 
+DATA_FILES = [("assets", ["assets/icon_active.png", "assets/icon_inactive.png"])]
+
 setup(
-	app=APP,
-	options={'py2app': OPTIONS},
-	setup_requires=['py2app'],
+    app=APP,
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
+	data_files=DATA_FILES,
 )
